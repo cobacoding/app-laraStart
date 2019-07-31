88699fdd0365cc7aed594dc543ad059c5ce5473d
unknown <codingsuka@gmail.com>
2019-08-01 06:55:31 +0700

    connection database

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -60,13 +60,44 @@
                     <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
+                <form @submit.prevent="createUser">
                 <div class="modal-body">
-                    ...
-                </div>
+                    <div class="form-group">
+                      <input v-model="form.name" type="text" name="name" placeholder="Name" 
+                      class="form-control" :class="{ 'is-valid':form.errors.has('name') }">
+                      <has-error :form="form" field="name"></has-error>
+                    </div>
+                    <div class="form-group">
+                      <input v-model="form.email" type="email" name="email" placeholder="Email Address" 
+                      class="form-control" :class="{ 'is-valid':form.errors.has('email') }">
+                      <has-error :form="form" field="email"></has-error>
+                    </div>
+                    <div class="form-group">
+                      <input v-model="form.bio" type="text" name="bio" id="id"  placeholder="Short bio for user(optional)" 
+                      class="form-control" :class="{ 'is-valid':form.errors.has('bio') }">
+                      <has-error :form="form" field="bio"></has-error>
+                    </div>
+                    <div class="form-group">
+                      <select name="type" v-model="form.type" id="type" class="form-control" :class="{'is-valid': form.errors.has('type') }">
+                        <option value="">Select User Role</option>
+                        <option value="admin">Admin</option>
+                        <option value="user">Standard User</option>
+                        <option value="author">Author</option>
+                      </select>
+                      <has-error :form="form" field="type"></has-error>
+                    </div>
+                    <div class="form-group">
+                      <input v-model="form.password" type="password" name="password" id="password" 
+                      class="form-control" :class="{ 'is-valid':form.errors.has('password') }">
+                      <has-error :form="form" field="password"></has-error>
+                    </div>
+                </div>   
+        
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Create</button>
+                    <button type="submit" class="btn btn-primary">Create</button>
                 </div>
+                </form> 
                 </div>
             </div>
         </div>
@@ -76,6 +107,23 @@
 
 <script>
     export default {
+      data() {
+        return {
+          form: new Form({
+            name: '',
+            email: '',
+            password: '',
+            type: '',
+            bio: '',
+            photo: ''
+          })
+        }
+      },
+      methods: {
+        createUser(){
+          this.form.post('api/user');
+        }
+      },
         mounted() {
             console.log('Component mounted.')
         }
